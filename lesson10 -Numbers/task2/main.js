81a6ab2badf58bfc3f847216b398fb8f680bc185
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-02 15:43:53 +0200

    js

---
+++ lesson10 -Numbers/task2/main.js
@@ -0,0 +1,7 @@
+const getParsedIntegers = arr => arr.map(num => Number.parseInt(num))
+
+const getParsedIntegersV2 = arr => arr.map(num => parseInt(num));
+
+const getParsedFloats = arr => arr.map(num => Number.parseFloat(num));
+
+const getParsedFloatsV2 = arr => arr.map(num => parseFloat(num));
