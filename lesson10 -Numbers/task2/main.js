767b769fcd4fafda68e9a29cd9ae41721582d859
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-02 15:48:25 +0200

    js

--- lesson10 -Numbers/task2/main.js
+++ lesson10 -Numbers/task2/main.js
@@ -5,3 +5,9 @@ const getParsedIntegersV2 = arr => arr.map(num => parseInt(num));
 const getParsedFloats = arr => arr.map(num => Number.parseFloat(num));
 
 const getParsedFloatsV2 = arr => arr.map(num => parseFloat(num));
+
+let parse = [17.123, 18.2, 19.9, 10.1238]
+console.log(getParsedIntegers(parse));
+console.log(getParsedIntegersV2(parse));
+console.log(getParsedFloats(parse));
+console.log(getParsedFloatsV2(parse));
