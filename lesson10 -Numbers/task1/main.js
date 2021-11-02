3216b57b0aa081f1d0f2a1f67338671485ad2e4b
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-02 15:32:22 +0200

    js

---
+++ lesson10 -Numbers/task1/main.js
@@ -0,0 +1,9 @@
+const getFiniteNumbers = arr => arr.filter(num => Number.isFinite(num));
+
+const getFiniteNumbers = arr => arr.filter(num => isFinite(num));
+
+const getNaN = arr => arr.filter(num => Number.isNaN(num))
+
+const getNaNV2 = arr => arr.filter(num => isNaN(num));
+
+const getIntegers = arr => arr.filter(num => Number.isInteger(num));
---
+++ lesson9-ObjectsKeys/task7/main2.js
@@ -0,0 +1,8 @@
+const getSubArray = (arr, numberOfElements) => {
+   let newArr = [];
+   for (let i = o; i <= numberOfElements.length; i++) {
+      
+   }
+}
+let numbers = getSubArray([11, 4, 8, 3], 2);
+console.log(numbers);
