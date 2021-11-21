c78850df8d27b76228eee80fc27041e1fd8a372f
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-21 14:02:31 +0200

    js

---
+++ lesson11-string/task1/main.js
@@ -0,0 +1,22 @@
+//put your code here
+// splitString('abcd efgh', 4) => 'abcd/n, efgh'
+let splitText = (str, len = 10) => {
+   if (typeof str !== 'string') {
+      return null;
+   }
+      const arr = [];
+      let startPosition = 0;
+
+   while (true) {
+      let chunk = str.substr(startPosition, len);
+      if (chunk.length === 0) {
+      break;
+    }
+      arr.push(chunk[0].toUpperCase() + chunk.slice(1));
+      startPosition += len;
+   }
+   return arr.join('\n');
+}
+  
+const str1 = 'asdfghjklm';
+console.log(splitText(12, 3));
