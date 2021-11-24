fe4106cce62ed75a3280c08d3ceabaf63dffff73
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-24 13:06:19 +0200

    js

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -1,7 +1,7 @@
 
 
 export const getSquaredNumbers = arr =>
-   arr.map(num => num * num);
+   arr.map(num => num ** 2);
 
 export const getOddNumbers = arr =>
    arr.filter(num => (num % 2 === 1));
