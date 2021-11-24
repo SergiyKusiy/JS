44a63d1446f9907b6c34cff572b80da9311b5307
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-24 13:10:00 +0200

    js

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -1,9 +1,7 @@
 
 
-export const getSquaredNumbers = arr =>
-   arr.map(num => num ** 2);
+export const getSquaredArray = arr => arr.map(elem => elem ** 2);
 
-export const getOddNumbers = arr =>
-   arr.filter(num => (num % 2 === 1));
+export const getOddNumbers = arr => arr.filter(elem => elem % 2 !== 0);
 
- export default (a, b) => a + b;
+export default (a, b) => a + b;
--- lesson13/task2/index.test.js
+++ lesson13/task2/index.test.js
@@ -1,17 +1,17 @@
 
-import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.js';
-
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
-   const result = getSquaredNumbers([1, 2, 3])
-   expect(result).toEqual([1, 4, 9]);
+  const result = getSquaredArray([1, 2, 3, 4, 5]);
+  expect(result).toEqual([1, 4, 9, 16, 25]);
 });
-it('should keep odd numbers only', () => {
-   const result = getOddNumbers([1, 2, 3, 4, 5])
-   expect(result).toEqual([1, 3, 5]);
-});
-it('should get sum of numbers', () => {
-   const result = getSum(8, 4);
 
-   expect(result).toEqual(12);
+it('should get only odd numbers', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+  expect(result).toEqual([1, 3, 5]);
+});
+
+it('should get sum of two numbers', () => {
+  const result = getSum(7, 7);
+  expect(result).toEqual(14);
 });
