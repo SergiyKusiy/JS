ac21af83480db19c586aabb61940fc11c3eb2dea
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-25 20:11:08 +0200

    js

---
+++ lesson15/task2/main.js
@@ -0,0 +1,43 @@
+export const createCalculator = () => {
+   let result = 0;
+ 
+   function getMemo() {
+     return result;
+   }
+ 
+   function add(num) {
+     result += num;
+   }
+   function decrease(num) {
+     result -= num;
+   }
+ 
+   function reset() {
+     result = 0;
+   }
+ 
+   return {
+     getMemo,
+     add,
+     decrease,
+     reset,
+   };
+ };
+ 
+ const calculator1 = createCalculator();
+ calculator1.add(57);
+ console.log(calculator1.getMemo());
+ calculator1.decrease(37);
+ console.log(calculator1.getMemo());
+ calculator1.reset();
+ console.log(calculator1.getMemo());
+ 
+ // =================================
+ 
+ const calculator2 = createCalculator();
+ calculator2.add(33);
+ console.log(calculator2.getMemo());
+ calculator2.decrease(50);
+ console.log(calculator2.getMemo());
+ calculator2.reset();
+ console.log(calculator2.getMemo());
