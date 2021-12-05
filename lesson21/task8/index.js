0a3534f293a19ef92badabecdf942ade6038c9b4
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-05 18:51:10 +0200

    js

---
+++ lesson21/task8/index.js
@@ -0,0 +1,5 @@
+export function createButton(buttonText) {
+  const buttonElem = document.createElement('button');
+  buttonElem.textContent = buttonText;
+  document.querySelector('body').append(buttonElem);
+}
