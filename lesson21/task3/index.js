64858c1fa3fedd76fbd661d770e45f585fcd32f6
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-05 17:31:58 +0200

    js

---
+++ lesson21/task3/index.js
@@ -0,0 +1,14 @@
+function getItemsList() {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+}
+
+export function getItemsArray() {
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.dir(elementsArray);
+  return elementsArray;
+}
+
+getItemsList();
+getItemsArray();
