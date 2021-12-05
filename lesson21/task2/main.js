be3be6f6dd643d9a7b6a8b71a12414618be175bd
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-05 16:54:33 +0200

    Update main.js

--- lesson21/task2/main.js
+++ lesson21/task2/main.js
@@ -1,10 +1,10 @@
-export function getTitleElement() {
+  function getTitleElement() {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 }
 
-export function getInputElement() {
+  function getInputElement() {
   const inputElem = document.querySelector('input[type="text"]');
   console.dir(inputElem);
   return inputElem;
