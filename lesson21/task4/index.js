13a66694d156917cc06a3038844fa623d243ae9a
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-05 18:01:37 +0200

    js

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,7 +1,6 @@
 export function getTitle() {
   return document.querySelector('.title').textContent;
 }
-<<<<<<< HEAD
 
 export function getDescription() {
   return document.querySelector('.about').innerText;
@@ -11,18 +10,9 @@ export function getPlans() {
   return document.querySelector('.plans').innerHTML;
 }
 
-=======
-export function getDescription() {
-  return document.querySelector('.about').innerText;
-}
-export function getPlans() {
-  return document.querySelector('.plans').innerHTML;
-}
->>>>>>> 6c5dee87dd34b4371e9a9afb4d6aa2b559368e4f
 export function getGoal() {
   return document.querySelector('.goal').outerHTML;
 }
-
 console.log(getTitle());
 console.log(getDescription());
 console.log(getPlans());
