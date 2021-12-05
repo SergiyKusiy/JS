a32e235b3f5e28ab310daedcf44da2cefd7d2885
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-05 18:00:24 +0200

    js

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,15 +1,19 @@
-function getTitle() {
+export function getTitle() {
   return document.querySelector('.title').textContent;
 }
-function getDescription() {
+
+export function getDescription() {
   return document.querySelector('.about').innerText;
 }
-function getPlans() {
+
+export function getPlans() {
   return document.querySelector('.plans').innerHTML;
 }
-function getGoal() {
+
+export function getGoal() {
   return document.querySelector('.goal').outerHTML;
 }
+
 console.log(getTitle());
 console.log(getDescription());
 console.log(getPlans());
