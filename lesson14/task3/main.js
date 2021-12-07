8bc457b0bf86e74f8af2fda391579da581213f0e
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-07 19:29:21 +0200

    js

--- lesson14/task3/main.js
+++ lesson14/task3/main.js
@@ -1,12 +1,15 @@
 let result = 0;
 
- const getMemo = () => result;
+const getMemo = () => result;
 
- const add = num => (result += num);
+// eslint-disable-next-line no-return-assign
+const add = num => (result += num);
 
- const decrease = num => (result -= num);
+// eslint-disable-next-line no-return-assign
+const decrease = num => (result -= num);
 
- const reset = () => (result = 0);
+// eslint-disable-next-line no-return-assign
+const reset = () => (result = 0);
 
 add(23);
 console.log(getMemo());
