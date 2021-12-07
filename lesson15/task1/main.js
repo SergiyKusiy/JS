bfd6361e1533ab76d4c7165ded0736e3bb4a4473
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-07 20:38:29 +0200

    js

--- lesson15/task1/main.js
+++ lesson15/task1/main.js
@@ -1,9 +1,10 @@
-export let makeCounter = () => {
-   let count = 0;
-   return function() {
-       return count ++;
-   }
-}
+export const makeCounter = () => {
+  let count = 0;
+  return function () {
+    // eslint-disable-next-line no-plusplus
+    return count++;
+  };
+};
 const counter1 = makeCounter();
 const counter2 = makeCounter();
 console.log(counter1()); // 0
--- lesson15/task3/main.js
+++ lesson15/task3/main.js
@@ -1,50 +1,50 @@
 function createLogger() {
-   const memory = [];
-  
-   let warn = text => {
-      memory.push({
-          message: text,
-          dateTime: new Date(),
-          type: 'warn',
-       })
-   }
-  
-   let error = text => {
-      memory.push({
-          message: text,
-          dateTime: new Date(),
-          type: 'error',
-       })
-   }
-  
-   let log = text => {
-      memory.push({
-          message: text,
-          dateTime: new Date(),
-          type: 'log',
-       })
-   }
-  
-   let getRecords = string => {
-       const sortMemory = memory.sort((a,b) => b.dateTime - a.dateTime);
-  
-       return string === undefined ? sortMemory : sortMemory.filter(elem => elem.type === string);
-   }
-   return {
-      warn,
-      error,
-      log,
-      getRecords,
-   }
-  }
-  
-  const logger = createLogger();
-  
-  console.log(logger.log('User logged in'));
-  console.log(logger.warn('User try to restricted page'));
-  console.log(logger.log('User logged out'));
-  console.log(logger.error('Unexpected error on the site'));
-  console.log(logger.getRecords());
-  console.log(logger.getRecords('log'));
-  console.log(logger.getRecords('error'));
-  console.log(logger.getRecords('warn'));
+  const memory = [];
+
+  const warn = text => {
+    memory.push({
+      message: text,
+      dateTime: new Date(),
+      type: 'warn',
+    });
+  };
+
+  const error = text => {
+    memory.push({
+      message: text,
+      dateTime: new Date(),
+      type: 'error',
+    });
+  };
+
+  const log = text => {
+    memory.push({
+      message: text,
+      dateTime: new Date(),
+      type: 'log',
+    });
+  };
+
+  const getRecords = string => {
+    const sortMemory = memory.sort((a, b) => b.dateTime - a.dateTime);
+
+    return string === undefined ? sortMemory : sortMemory.filter(elem => elem.type === string);
+  };
+  return {
+    warn,
+    error,
+    log,
+    getRecords,
+  };
+}
+
+const logger = createLogger();
+
+console.log(logger.log('User logged in'));
+console.log(logger.warn('User try to restricted page'));
+console.log(logger.log('User logged out'));
+console.log(logger.error('Unexpected error on the site'));
+console.log(logger.getRecords());
+console.log(logger.getRecords('log'));
+console.log(logger.getRecords('error'));
+console.log(logger.getRecords('warn'));
