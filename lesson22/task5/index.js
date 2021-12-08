e251b6bdb7412cb0d98b20831fef4806402e609a
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-08 14:23:46 +0200

    js

---
+++ lesson22/task5/index.js
@@ -0,0 +1,5 @@
+const searchBtn = document.querySelector('search__btn');
+const inputText = document.querySelector('search__input');
+searchBtn.addEventListener('click', () => {
+  console.log(inputText.value);
+});
