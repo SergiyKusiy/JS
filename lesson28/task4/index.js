c071f5e0883ffc2103db6d3ac9070460ebffb24c
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-14 19:11:16 +0200

    js

--- lesson28/task4/index.js
+++ lesson28/task4/index.js
@@ -1,15 +1,13 @@
 // eslint-disable-next-line no-undef
-
-const add3 = value => value + 3;
-const mult2 = value => value * 2;
-const div4 = value => value / 4;
-
-// eslint-disable-next-line no-use-before-define
-const doEverything = compose(add3, mult2, div4);
-
 const compose =
   (...funcs) =>
-  value => {
-    return funcs.reduce((acc, funcs) => funcs(acc), value);
-  };
-console.log(doEverything(3));
+  value =>
+    funcs.reduce((acc, func) => func(acc), value);
+
+const add2 = value => value + 2;
+const square = value => value * value;
+const half = value => value / 2;
+
+const superFunc = compose(add2, square, half);
+const res = superFunc(2); // 8
+console.log(res);
