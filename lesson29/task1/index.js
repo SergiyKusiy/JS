b528b4a47f13769cd51399132b3dd186bb8a605d
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-20 16:24:47 +0200

    js

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,35 +1,33 @@
-export const addImage = (imgSrc, callback) => {
+const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
   imgElem.setAttribute('alt', 'My Photo');
   imgElem.src = imgSrc;
-
   const containerElem = document.querySelector('.page');
   containerElem.append(imgElem);
 
   const onImageLoaded = () => {
     callback(null, imgElem);
-    // const { width, height } = imgElem;
-    // callback(null, { width, height });
   };
 
   imgElem.addEventListener('load', onImageLoaded);
-
-  imgElem.addEventListener('error', () => callback('Image load is failed...'));
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-// callack example
+// callack function
 const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
+
   const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
+
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// test call
+// examples
 addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Arial-View-Of-Blue-Lakes-And--227291596.jpg',
   onImageLoaded,
 );
