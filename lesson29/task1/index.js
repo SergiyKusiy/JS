91c3c9d4fa079e3e4afbc8b08f9291ef3d6d6f0d
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-20 16:16:52 +0200

    js

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,26 +1,22 @@
-/* eslint-disable no-param-reassign */
-export const addImage = (imgSrc, callback) => {
-  const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
+const addImage = (imgSrc, callback) => {
+  const imageElem = document.createElement('img');
+  imageElem.setAttribute('alt', 'My photo');
+  imageElem.src = imgSrc;
 
-  const onImageLoaded = () => {
-    callback(null, imgElem);
-  };
+  const container = document.querySelector('.page');
+  container.append(imageElem);
 
-  imgElem.addEventListener('load', onImageLoaded);
-  imgElem.addEventListener('error', () => callback('Image load is failed...'));
+  imageElem.addEventListener('load', () => callback(null, imageElem));
+  imageElem.addEventListener('error', () => callback('Image load is failed...'));
 };
 
 // callack example
-const onImageLoaded = (error, data) => {
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = data;
+  const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
