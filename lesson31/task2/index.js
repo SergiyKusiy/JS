315683cefea7e63724095caa848f2e05eb25cf9d
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-21 23:34:22 +0200

    js

---
+++ lesson31/task2/index.js
@@ -0,0 +1,30 @@
+export const asyncCalculator = number => {
+  const p = new Promise(resolve => {
+    setTimeout(() => {
+      console.log(`Initial value: ${number}`);
+      resolve(number);
+    }, 500);
+  })
+    .then(
+      value =>
+        new Promise(resolve => {
+          const result = value ** 2;
+          setTimeout(() => {
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+          }, 500);
+        }),
+    )
+    .then(
+      value =>
+        new Promise(resolve => {
+          const result = value * 2;
+          console.log(`Doubled value: ${result}`);
+          resolve(result);
+        }),
+    );
+
+  return p;
+};
+
+asyncCalculator(5);
