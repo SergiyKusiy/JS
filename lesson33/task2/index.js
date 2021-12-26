33ecb58be5088a5fa82974dfdd77ef2a4b4bf256
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-12-26 19:00:11 +0200

    js

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,13 +1,9 @@
-/* eslint-disable consistent-return */
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-export function getTasksList() {
-  return fetch(baseUrl).then(response => response.json());
-}
+export const getTasksList = () => fetch(`${baseUrl}`).then(response => response.json());
 
-export function getTaskById(taskId) {
-  return fetch(`${baseUrl}/${taskId}`).then(response => response.json());
-}
+export const getTaskById = taskId =>
+  fetch(`${baseUrl}/${taskId}`).then(response => response.json());
 
 // examples
 getTasksList().then(tasksList => {
