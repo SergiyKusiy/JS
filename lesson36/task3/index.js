6d32c00afbb07c7d4d1cab7f2b5b2feab0459f70
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-01-24 22:31:21 +0200

    js

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-return-await */
-export const getUserBlogs = async users => {
-  try {
-    const fetchUser = userId =>
-      fetch(`https://api.github.com/users/${userId}`).then(res => res.json());
+const fetchUser = async userId => {
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (response.ok) {
+    return response.json();
+  }
 
-    return await Promise.all(users.map(userId => fetchUser(userId).then(user => user.blog)));
-  } catch (error) {
-    throw new Error('Wrong');
+  throw new Error('Failed to fetch user');
+};
+
+// eslint-disable-next-line consistent-return
+export const getUsersBlogs = user => {
+  try {
+    return Promise.all(user.map(el => fetchUser(el).then(response => response.blog)));
+  } catch (err) {
+    alert(err.message);
   }
 };
+
+getUsersBlogs(['google', 'facebook', 'gaearon']).then(linkList => console.log(linkList));
 
 // getUserBlogs(['google', 'zoom']).then(res => console.log(res));
